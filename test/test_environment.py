8bee0aa76d3821e1754b17a9c2865031e62e35db
Johan Hidding <j.hidding@esciencecenter.nl>
2023-03-28 11:54:37 +0200

    fixing environment files

--- test/test_environment.py
+++ test/test_environment.py
@@ -1,7 +1,6 @@
 import numpy as np
 import scipy
 import dask
-import snakemake
 import numba
 from packaging.version import parse as version
 
@@ -10,5 +9,4 @@ def test_library_versions():
     assert version(np.__version__) >= version("1.15")
     assert version(scipy.__version__) >= version("1.5")
     assert version(dask.__version__) >= version("2.20")
-    assert version(snakemake.__version__) >= version("5.28")
     assert version(numba.__version__) >= version("0.50")
--- test/test_snakemake.py
+++
@@ -1,8 +0,0 @@
-from subprocess import run
-from shutil import copy
-
-
-def test_snakemake(tmp_path):
-    copy("./test/hello/Snakefile", tmp_path)
-    run(["snakemake", "-j1"], cwd=tmp_path, check=True)
-    assert (tmp_path / "combined.txt").exists()
